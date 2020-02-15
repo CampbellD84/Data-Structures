a1700e0d160bbc08b3b37f6b040c6a87968abb9a
Douglas Campbell <dacampbelltbone@gmail.com>
2020-02-15 14:09:10 -0500

    Complete LRU Cache. Tests all passed.

--- lru_cache/doubly_linked_list.py
+++ lru_cache/doubly_linked_list.py
@@ -11,6 +11,7 @@ class ListNode:
     """Wrap the given value in a ListNode and insert it
     after this node. Note that this node could already
     have a next node it is point to."""
+
     def insert_after(self, value):
         current_next = self.next
         self.next = ListNode(value, self, current_next)
@@ -20,6 +21,7 @@ class ListNode:
     """Wrap the given value in a ListNode and insert it
     before this node. Note that this node could already
     have a previous node it is point to."""
+
     def insert_before(self, value):
         current_prev = self.prev
         self.prev = ListNode(value, current_prev, self)
@@ -28,6 +30,7 @@ class ListNode:
 
     """Rearranges this ListNode's previous and next pointers
     accordingly, effectively deleting this ListNode."""
+
     def delete(self):
         if self.prev:
             self.prev.next = self.next
@@ -49,25 +52,95 @@ class DoublyLinkedList:
         return self.length
 
     def add_to_head(self, value):
-        pass
+        # create new node
+        new_node = ListNode(value)
+        # increment length of DLL
+        self.length += 1
+        # set head/tail to new ListNode if none
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        # set the prev/next pointers
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
 
     def remove_from_head(self):
-        pass
+        # set value to equal head ListNode value
+        value = self.head.value
+        # call delete on the tail
+        self.delete(self.head)
+        return value
 
     def add_to_tail(self, value):
-        pass
+        # if self.length > 0:
+            # self.head.insert_before(value)
+            # self.head = self.head.prev
+        # elif self.length == 0:
+            # self.head = ListNode(value)
+            # self.tail = self.head
+            # self.length += 1
+        # create new ListNode
+        new_node = ListNode(value)
+        # increment length of DLL
+        self.length += 1
+        # if new ListNode is not head and not tail
+        # set new ListNode as head and tail if only ListNode in DLL
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            # pointer to new node
+            self.tail = new_node
 
     def remove_from_tail(self):
-        pass
+        # set value to equal tail ListNode value
+        value = self.tail.value
+        # call delete on the tail
+        self.delete(self.tail)
+        return value
 
     def move_to_front(self, node):
-        pass
+        if node is not self.head:
+            self.delete(node)
+            self.add_to_head(node.value)
 
     def move_to_end(self, node):
-        pass
+        # if ListNode is tail in DLL, return
+        if node is self.tail:
+            return
+        # Set value of ListNode
+        value = node.value
+        # delete ListNode
+        self.delete(node)
+        # Add ListNode to tail of DDL
+        self.add_to_tail(value)
 
     def delete(self, node):
-        pass
+        if not self.head and not self.tail:
+            return
+        elif self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif node.prev is None:
+            self.head = node.next
+        elif node == self.tail:
+            self.tail = node.prev
+        node.delete()
+        self.length -= 1
 
     def get_max(self):
-        pass
+        max_value = None
+        temp_var = None
+
+        temp_var = max_value = self.head
+
+        while temp_var != None:
+            if temp_var.value > max_value.value:
+                max_value = temp_var
+            temp_var = temp_var.next
+
+        return max_value.value
--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,6 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +9,15 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        # storage dict
+        self.store = {}
+        self.limit = limit
+        # current number of node LRU cache has
+        self.node_num = 0
+        # DLL to hold key and val pair in correct order
+        self.order_track = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,9 +26,16 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
-    def get(self, key):
-        pass
 
+    def get(self, key):
+        # if val at key not in cache, return None
+        if key not in self.store:
+            return None
+        # if present, move to end of DLL and return value
+        get_node = self.store[key]
+        # move key val pair to end of DLL
+        self.order_track.move_to_end(get_node)
+        return get_node.value[1]
     """
     Adds the given key-value pair to the cache. The newly-
     added pair should be considered the most-recently used
@@ -29,5 +46,25 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+        # if key in store dict
+        if key in self.store:
+            # assign value at key
+            node = self.store[key]
+            node.value = (key, value)
+            # move to end of DLL
+            self.order_track.move_to_end(node)
+            return
+        self.order_track.add_to_tail((key, value))
+        self.store[key] = self.order_track.tail
+        # increment node count
+        self.node_num += 1
+        # if node count is greater than limit
+        if self.node_num > self.limit:
+            # delete item in store
+            del self.store[self.order_track.head.value[0]]
+            # remove from DLL
+            self.order_track.remove_from_head()
+            # decrement the node count
+            self.node_num -= 1
